40d6becc9ce7632fced728d864b098997721c2e2
danteWebCoder <danteonecoder@gmail.com>
2026-07-27 21:13:34 +0200

    style(landing): info add layout

--- src/pages/landing/scrollControl.js
+++ src/pages/landing/scrollControl.js
@@ -1,27 +1,51 @@
+/* menu */
 const menu = document.querySelector("#landingMenu")
 const wellcome = document.querySelector(".wellcome")
 const menuHeight = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--menuHeight"))
 const wellcomeHeight = parseFloat(window.getComputedStyle(wellcome).getPropertyValue("height"))
 const menuTime = parseFloat(window.getComputedStyle(menu).getPropertyValue("transition")) * 1000
-const visibilityPoint = wellcomeHeight * 0.8 - menuHeight
+/* info image */
+/* const infoSection = document.querySelector("#infoSection")
+const infoSection_height = parseFloat(getComputedStyle(infoSection).getPropertyValue("height"))
+ */const image = document.querySelector("#imageBox .image")
+
 
 const doVisible = async () => {
     menu.classList.remove("hidden")
     await new Promise(resolve => setTimeout(resolve, 10))
     menu.style.opacity = 1
-    visible = true
 }
 
 const doHidden = async () => {
     menu.style.opacity = 0
     await new Promise(resolve => setTimeout(resolve, menuTime))
     menu.classList.add("hidden")
-    visible = false
 }
 
-let visible = false
+const fadeIn_imageInfo = () => {
+
+}
+
+let pos = 0
 window.addEventListener("scroll", () => {
     const posY = window.scrollY
-    if (posY >= visibilityPoint && !visible) doVisible()
-    if (posY < visibilityPoint && visible) doHidden()
+
+    /* menu */
+    if (posY >= wellcomeHeight - menuHeight && posY > pos) {
+        doVisible()
+        pos = posY
+    }
+    if (posY <= wellcomeHeight && posY < pos) {
+        doHidden()
+        pos = posY
+    }
+    /* info image */
+    if (posY >= wellcomeHeight) {
+        image.classList.replace("imageMin", "imageMax")
+        pos = posY
+    }
+    if (posY < wellcomeHeight) {
+        image.classList.replace("imageMax", "imageMin")
+        pos = posY
+    }
 })
