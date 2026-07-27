9a6a308bfd89fe9126e8b5f0c6eada90a064eca8
danteWebCoder <danteonecoder@gmail.com>
2026-07-27 09:35:09 +0200

    feat: add menu visibility via scroll and add new menu logo

--- src/pages/landing/scrollControl.js
+++ src/pages/landing/scrollControl.js
@@ -1,18 +1,27 @@
-const menu = document.querySelector("#menu")
-const maxHeight = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--menuHeight_max"))
-let menuState = "max"
+const menu = document.querySelector("#landingMenu")
+const wellcome = document.querySelector(".wellcome")
+const menuHeight = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--menuHeight"))
+const wellcomeHeight = parseFloat(window.getComputedStyle(wellcome).getPropertyValue("height"))
+const menuTime = parseFloat(window.getComputedStyle(menu).getPropertyValue("transition")) * 1000
+const visibilityPoint = wellcomeHeight * 0.8 - menuHeight
 
-console.log(maxHeight)
-
-const minimizeMenu = (posY) => {
-    if (menuState === "max") {
-        if (posY >= maxHeight) {
-            menu.classList.replace("menu_maxHeight", "menu_minHeight")
-        }
-    }
+const doVisible = async () => {
+    menu.classList.remove("hidden")
+    await new Promise(resolve => setTimeout(resolve, 10))
+    menu.style.opacity = 1
+    visible = true
 }
 
+const doHidden = async () => {
+    menu.style.opacity = 0
+    await new Promise(resolve => setTimeout(resolve, menuTime))
+    menu.classList.add("hidden")
+    visible = false
+}
+
+let visible = false
 window.addEventListener("scroll", () => {
     const posY = window.scrollY
-    console.log(posY)
+    if (posY >= visibilityPoint && !visible) doVisible()
+    if (posY < visibilityPoint && visible) doHidden()
 })
