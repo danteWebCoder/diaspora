49d641ef61a94ac3eaa20ef2ffae7f6926ef32dd
danteWebCoder <danteonecoder@gmail.com>
2026-07-30 09:16:28 +0200

    refactor(landing): info add grid layout

--- src/pages/landing/scrollControl.js
+++ src/pages/landing/scrollControl.js
@@ -3,11 +3,11 @@ const menu = document.querySelector("#menuBox")
 const wellcome = document.querySelector(".wellcome")
 const menuHeight = parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--menuHeight"))
 const wellcomeHeight = parseFloat(window.getComputedStyle(wellcome).getPropertyValue("height"))
-const menuTime = parseFloat(window.getComputedStyle(menu).getPropertyValue("transition")) * 1000
-/* info image */
-const image = document.querySelector("#imageBox .image")
+/* const menuTime = parseFloat(window.getComputedStyle(menu).getPropertyValue("transition")) * 1000
+ *//* info image */
+/* const image = document.querySelector("#imageBox .image")
 const imageTime = parseFloat(window.getComputedStyle(image).getPropertyValue("transition")) * 1000
-
+ */
 const doVisible = async () => {
     menu.classList.remove("hidden")
     menu.classList.add("menuVisible")
@@ -17,13 +17,12 @@ const doVisible = async () => {
 
 const doHidden = async () => {
     menu.style.opacity = 0
-    await new Promise(resolve => setTimeout(resolve, menuTime))
+    await new Promise(resolve => setTimeout(resolve, 10))
     menu.classList.add("hidden")
     menu.classList.remove("menuVisible")
 }
 
-const fadeIn_imageInfo = async () => {
-
+/* const fadeIn_imageInfo = async () => {
     image.classList.add("image300")
     await new Promise(resolve => setTimeout(resolve, imageTime))
 
@@ -34,24 +33,24 @@ const fadeIn_imageInfo = async () => {
     image.classList.remove("image100")
     image.classList.add("imageMax")
 }
-
+ */
 let pos = 0
 let imageInfo_opened = false
 window.addEventListener("scroll", async () => {
     const posY = window.scrollY
 
     /* menu */
-    if (posY >= wellcomeHeight && posY > pos) {
+    if (posY > wellcomeHeight && posY > pos) {
         doVisible()
     }
     if (posY <= wellcomeHeight && posY < pos) {
         doHidden()
     }
     /* info image */
-    if (posY >= wellcomeHeight && !imageInfo_opened) {
+/*     if (posY >= wellcomeHeight && !imageInfo_opened) {
         imageInfo_opened = true
         fadeIn_imageInfo()
     }
-
+ */
     pos = posY
 })
