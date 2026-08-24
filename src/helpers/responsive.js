c3b89318b51b0e8dc97450afa5269e579a848422
danteWebCoder <danteonecoder@gmail.com>
2026-08-24 22:41:05 +0200

    feat(core): añadido script autoresposive con html declarativo

---
+++ src/helpers/responsive.js
@@ -0,0 +1,26 @@
+const mediaQueries = document.documentElement.getAttribute("data-responsive").split(" ").map(Number).sort((a, b) => b - a)
+let mediaQueryActual = mediaQueries.findLast(item => window.innerWidth <= item) || null
+
+window.addEventListener("resize", (e) => {
+    const query = mediaQueries.findLast(item => window.innerWidth <= item)
+    if (query && mediaQueryActual !== query) {
+        mediaQueryActual = query
+        console.log(mediaQueryActual)
+        cambiarMedia(mediaQueryActual)
+    }
+})
+
+const aplicarClases = (elemento, add, remove) => {
+    add && add.forEach(item => elemento.classList.add(item))
+    remove && remove.forEach(item => elemento.classList.remove(item))
+}
+
+const cambiarMedia = (media) => {
+    const elementos = document.querySelectorAll(`[media-${media}]`)
+    elementos.forEach(elemento => {
+        const clases = elemento.getAttribute(`media-${media}`)
+        const add = clases.match(/add:\s*\[([^\]]+)\]/)?.[1]?.split(",").map(c => c.trim()) || []
+        const remove = clases.match(/remove:\s*\[([^\]]+)\]/)?.[1]?.split(",").map(c => c.trim()) || []
+        aplicarClases(elemento, add, remove)
+    })
+}
