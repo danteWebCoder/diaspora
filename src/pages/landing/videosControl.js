fe68a345b6ef48f13e1c4121fe8637ae4dc14713
danteWebCoder <danteonecoder@gmail.com>
2026-08-28 17:04:38 +0200

    feat(landing): videos responsive

--- src/pages/landing/videosControl.js
+++ src/pages/landing/videosControl.js
@@ -1,41 +1,40 @@
-const miniaturasVideos = document.querySelectorAll("img[alt='miniaturaVideo']")
-const videos = {
-    0: {
-        enlace: `<iframe class="max" src="https://www.youtube.com/embed/E-E2wjfvbEE?si=4cwaqcL9twEyrbWU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
-        titulo: "Titulo 0",
-        info: "info video 0"
-    },
-    1: {
-        enlace: `<iframe class="max" src="https://www.youtube.com/embed/CF2fwuf9TNk?si=8NpgUP1p_KH4FAh4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
-        titulo: "Titulo 1",
-        info: "info video 1"
-    },
-    2: {
-        enlace: `<iframe class="max" src="https://www.youtube.com/embed/bocxIfCGB7g?si=4_PqmSOBMvCpjvtB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
-        titulo: "Titulo 2",
-        info: "info video 2"
-    },
-    3: {
-        enlace: `<iframe class="max" src="https://www.youtube.com/embed/Zik1cAnvVT0?si=jCHGXShpZWMBgoIp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
-        titulo: "Titulo 3",
-        info: "info video 3"
+const cajasMiniaturas = [...document.querySelectorAll(".cajaMiniatura")]
+const miniaturas = [...cajasMiniaturas].map(caja => getComputedStyle(caja).getPropertyValue("background-image"))
+const reproductor = document.querySelector("#cajaReproductor")
+
+console.log(miniaturas, cajasMiniaturas)
+
+const videos = [
+    `<iframe class="max" src="https://www.youtube.com/embed/E-E2wjfvbEE?si=4cwaqcL9twEyrbWU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+    `<iframe class="max" src="https://www.youtube.com/embed/CF2fwuf9TNk?si=8NpgUP1p_KH4FAh4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+    `<iframe class="max" src="https://www.youtube.com/embed/bocxIfCGB7g?si=4_PqmSOBMvCpjvtB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+]
+
+const restaurarMiniaturas = (indexSeleccion) => {
+    cajasMiniaturas.forEach((item, index) => {
+        if (index !== indexSeleccion) {
+            console.log(item)
+            item.innerHTML = ""
+        }
+    })
+}
+
+const cambiarVideo = (index, mobile) => {
+    !mobile && (reproductor.innerHTML = videos[index])
+    if (mobile) {
+        restaurarMiniaturas(index)
+        cajasMiniaturas[index].innerHTML = videos[index]
     }
 }
 
-
-const titulo = document.querySelector("#tituloVideo")
-const info = document.querySelector("#textoVideo")
-
-const cambiarVideo = (index) => {
-    const reproductor = document.querySelector("#cajaReproductor")
-    reproductor.innerHTML = videos[index].enlace
-    titulo.textContent = videos[index].titulo
-    info.textContent = videos[index].info
-}
-
-
-
-miniaturasVideos.forEach(radio => { radio.addEventListener("click", (e) => cambiarVideo(Array.from(miniaturasVideos).indexOf(e.target))) })
+cajasMiniaturas.forEach(caja => {
+    caja.addEventListener("click", (e) => {
+        const mobile = window.innerWidth <= 700 ? true : false
+        console.log(mobile)
+        cambiarVideo(Array.from(cajasMiniaturas).indexOf(e.target), mobile)
+    })
+})
 
 /* video por defecto */
-miniaturasVideos[0].dispatchEvent(new Event("click"))
+const mobile = window.innerWidth <= 700 ? true : false
+cajasMiniaturas[0].dispatchEvent(new Event("click"))
