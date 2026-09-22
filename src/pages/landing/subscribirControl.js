d6cc68bd57d64c507bdbfcefb5af51e9130a39d0
danteWebCoder <danteonecoder@gmail.com>
2026-09-22 22:45:32 +0200

    feat(landing): seccion subscritete añadido control

--- src/pages/landing/subscribirControl.js
+++ src/pages/landing/subscribirControl.js
@@ -1,3 +1,18 @@
 const mail = document.querySelector("#mail")
 const condiciones = document.querySelector("#condicionesInput")
+const enviar = document.querySelector("#enviar")
 
+const validar = (email) => {
+    const validacion = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/
+    return validacion.test(email) && condiciones.checked
+}
+
+const alternarBotonEnvio = () => {
+    const valido = validar(mail.value)
+    valido && enviar.classList.add("botonActivado")
+    !valido && enviar.classList.remove("botonActivado")
+    enviar.disabled = !valido
+}
+
+mail.addEventListener("input", () => alternarBotonEnvio())
+condiciones.addEventListener("input", () => alternarBotonEnvio())
