474ebf2b9b22c0a28eb2f93f6fcaff76473e6303
danteWebCoder <danteonecoder@gmail.com>
2026-09-23 23:29:57 +0200

    fix(landing): botom menu pantalla cambio de color por estados css y scroll

--- src/pages/landing/scrollControl.js
+++ src/pages/landing/scrollControl.js
@@ -7,11 +7,13 @@ const fondoEstatico = document.querySelector("#fondoEstatico")
 const fondo1 = getComputedStyle(document.documentElement).getPropertyValue("--landingFondo1")
 const fondo2 = getComputedStyle(document.documentElement).getPropertyValue("--landingFondo2")
 const menuFondo = document.querySelector("#menuFondo")
+const lineasBotonMenu = document.querySelectorAll(".lineaBoton")
 
 const mostrarMenu = async () => {
     menu.classList.remove("noVisible")
     menu.classList.add("menuVisible")
     await new Promise(resolve => setTimeout(resolve, 10))
+    lineasBotonMenu.forEach(item => item.classList.add("lineaBotonNegra"))
     menu.style.opacity = 1
 }
 
@@ -20,6 +22,7 @@ const ocultarMenu = async () => {
     await new Promise(resolve => setTimeout(resolve, 10))
     menu.classList.add("noVisible")
     menu.classList.remove("menuVisible")
+    lineasBotonMenu.forEach(item => item.classList.remove("lineaBotonNegra"))
 }
 
 /* estadisticas */
