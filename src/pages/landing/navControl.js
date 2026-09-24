6be67d15f5ffca2ab7757f1e104c4e63dac365fd
danteWebCoder <danteonecoder@gmail.com>
2026-09-24 23:59:04 +0200

    refactor(landing): nav - simplificado control nav subMenus con altura dinamica

--- src/pages/landing/navControl.js
+++ src/pages/landing/navControl.js
@@ -1,7 +1,13 @@
 const projects = document.querySelector("#proyectos")
-const menus = [projects]
+const menus = {
+    "proyectos": {
+        boton: document.querySelector("#proyectos"),
+        subMenu: document.querySelector("#subMenuProyectos")
+    }
+}
 
-const alternarMenu = async (menu, menuItems, abierto) => {
+
+const alternarMenu = async (menu, menuItems, estado) => {
     const menuTempo = parseFloat(getComputedStyle(menu).getPropertyValue("transition")) * 1000
     const itemHeight = parseFloat(getComputedStyle(menuItems[0]).getPropertyValue("height"))
     const subMenuLista = menu.querySelector(".subMenuLista")
@@ -12,7 +18,7 @@ const alternarMenu = async (menu, menuItems, abierto) => {
     /* dinamic */
     const subMenuAlturaDinamica = (menuItems.length * itemHeight) + subMenuTop * 2 + "px"
 
-    if (abierto) {
+    if (estado) {
         menu.style.opacity = 1
         menu.style.height = subMenuAlturaDinamica
         await new Promise(resolve => setTimeout(resolve, menuTempo))
@@ -37,7 +43,11 @@ const moverItemsLista = async (items, toRight) => {
     }
 }
 
-menus.forEach(item => {
+/* menus.forEach(item => {
+    item.addEventListener("click", (e) => {
+        console.log
+    })
+
     item.addEventListener("mouseover", async (e) => {
         const menu = document.querySelector(`#${e.target.id}-subMenu`) || null
         const menuItems = menu?.querySelectorAll("li") || null
@@ -48,5 +58,38 @@ menus.forEach(item => {
         const menu = document.querySelector(`#${e.target.id}-subMenu`) || null
         const menuItems = menu?.querySelectorAll("li")
         menuItems && await alternarMenu(menu, menuItems, false)
+    })
+})
+ */
+
+
+const obtenerEstado = (subMenu) => {
+    return subMenu.dataset.abierto
+}
+
+const alternarEstado = (estado, subMenu, alturaSubmenu) => {
+    if (estado === "true") {
+        delete subMenu.dataset.abierto
+        subMenu.style.opacity = "0"
+        subMenu.style.height = "0"
+    } else {
+        subMenu.dataset.abierto = true
+        subMenu.style.opacity = "1"
+        subMenu.style.height = alturaSubmenu + "px"
+    }
+}
+
+const calcularAltura = (subMenu) => {
+    const items = subMenu.querySelectorAll(".subItem").length
+    const altura = subMenu.querySelector(".subItem").offsetHeight
+    return 40 + altura * items
+}
+
+Object.values(menus).forEach(item => {
+    item.boton.addEventListener("click", (e) => {
+        const subMenu = menus[e.target.id].subMenu
+        const estado = obtenerEstado(subMenu)
+        const alturaSubmenu = calcularAltura(subMenu)
+        alternarEstado(estado, subMenu, alturaSubmenu)
     })
 })
