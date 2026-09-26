e998e2486332cb83ffb8723efcbd6e3515ead5a2
danteWebCoder <danteonecoder@gmail.com>
2026-09-26 03:35:17 +0200

    refactor(helper): reactivo - crea un objeto plano autoreactivo, actualizable dinamicamente

--- src/helpers/reactividad.js
+++ src/helpers/reactividad.js
@@ -1,9 +1,29 @@
-export const reaccionar = (obj, callbacks) => {
-    return new Proxy(obj, {
+export const reactivo = (objeto, arrayFn) => {
+
+    const propFn = {}
+    propFn[Object.keys(objeto)[0]] = arrayFn
+
+    const proxy = new Proxy(objeto, {
         set(obj, prop, value) {
             obj[prop] = value
-            callbacks.forEach(item => item(value))
+            propFn[prop]?.forEach(fn => fn(value))
             return true
         }
     })
+
+    proxy.incluir = (nuevaProp, nuevoArrayFn) => {
+        const nombreProp = Object.keys(nuevaProp)[0]
+        objeto[nombreProp] ??= nuevaProp[nombreProp]
+        propFn[nombreProp] ??= []
+        nuevoArrayFn.forEach(fn => propFn[nombreProp].push(fn))
+    }
+
+    proxy.propiedades = () => Object.fromEntries(Object.entries(objeto).filter(item => typeof objeto[item[0]] !== "function"))
+
+    proxy.acciones = (prop = null) => {
+        if (prop && !propFn[prop]) return null
+        return prop ? propFn[prop] : propFn
+    }
+
+    return proxy
 }
--- src/pages/landing/controles/monitorViewPort.js
+++ src/pages/landing/controles/monitorViewPort.js
@@ -1,7 +1,19 @@
-export const display = {
-    scroll: window.scrollY,
-    altura: window.innerHeight
-}
+import { reactivo } from "../../../helpers/reactividad.js"
 
-window.addEventListener("scroll", () =>  display.scroll = window.scrollY)
-window.addEventListener("resize", () =>  display.altura = window.innerHeight)
+const fn1 = () => console.log("scroll")
+const fn2 = () => console.log("resize")
+const fn3 = () => console.log("fn")
+
+
+export const viewPort = reactivo({"scroll": window.scrollY}, [fn1])
+console.log(viewPort)
+viewPort.incluir({"altura": window.innerHeight}, [fn2]) 
+console.log(viewPort)
+viewPort.incluir({"altura": window.innerHeight}, [fn3]) 
+console.log(viewPort)
+console.log(viewPort.propiedades())
+console.log(viewPort.acciones("altura"))
+
+
+window.addEventListener("scroll", () =>  viewPort.scroll = window.scrollY)
+window.addEventListener("resize", () =>  viewPort.altura = window.innerHeight)
